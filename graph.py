66f8ee84fa767e13929080cc143d679818ec085b
Stepan Shabalin <stomperhomp@gmail.com>
2021-05-16 21:14:57 +0300

    working version

--- graph.py
+++ graph.py
@@ -3,13 +3,14 @@ import sys
 import numpy as np
 
 w = 56
-for i in sys.argv[1:]:
+for z, i in enumerate(sys.argv[1:]):
  x = eval(open(f"loss-{i}.txt").read())
  x = [np.convolve(np.array(b), np.ones(w), 'valid') / w for b in zip(*x)] 
+ f = ['-', '--'][z-1]
  for j, s in enumerate(x):
   #s = np.array(s)
   #s = np.convolve(s, np.ones(w), 'valid') / w
-  plt.plot(s, label=i+'-'+str(j+1))
- plt.plot(np.mean(x, axis=0), label=i+' total')
+  plt.plot(s, f, label=i+'-'+str(j+1))
+ plt.plot(np.mean(x, axis=0), f, label=i+' total')
 plt.legend()
 plt.show()
